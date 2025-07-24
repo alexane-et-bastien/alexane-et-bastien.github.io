0a950a46a722f7ed4278817f81867ff13eef56ed
bschnitzler <bastien.schnitzler@live.fr>
2025-07-24 19:53:07 +0200

    Added picture ULM, linked the forms for anwsers

--- assets/js/pwd_form.js
+++ assets/js/pwd_form.js
@@ -1,11 +1,13 @@
 function checkRSVPPassword() {
     const input = document.getElementById('rsvp-password').value.trim().toLowerCase();
-    if (input === 'pastel') {
-      window.location.href = 'https://www.google.com';
+    if (input === 'rose') {
+      window.open('https://docs.google.com/forms/d/e/1FAIpQLSc92ZaztbXB6sX_CywdZAtKwgx4Cu6X3gvoHiIMgfXPZD7fcQ/viewform?usp=dialog', '_blank');
+    } else if (input === 'pastel') {
+      window.open('https://docs.google.com/forms/d/e/1FAIpQLSebuNM9_aIp2aHMoFnrwg1gX6GsSEj6UKoDNaHgSW0tfQbTfg/viewform?usp=dialog', '_blank');
     } else if (input === 'lauragais') {
-      window.location.href = 'https://www.google.com';
+      window.open('https://docs.google.com/forms/d/e/1FAIpQLSfxkYQjmI6mccnu_U3RU6GQuwE1lRPx6DFmrl3P_N9HMT3iUw/viewform?usp=dialog', '_blank');
     } else if (input === 'cocagne') {
-      window.location.href = 'https://www.google.com';
+      window.open('https://docs.google.com/forms/d/e/1FAIpQLSe7gNXYD7UKkDJ_QP3aL4boPVn9w24J1cpJemSE2bogATU7-w/viewform?usp=dialog', '_blank');
     } else {
       document.getElementById('rsvp-error').style.display = 'block';
     }
